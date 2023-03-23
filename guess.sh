ba94fd41e65a700d3a33adfe9ed89e63c86e56f3
sebastianplantardo <128522102+sebastianplantardo@users.noreply.github.com>
2023-03-23 16:30:49 +0100

    Update guess.sh

--- guess.sh
+++ guess.sh
@@ -14,26 +14,11 @@
 # Output : display if the integer given by player is lower, equal to or higher than the mystery number. Up to 10 attempts.
 #
 ####################
-
-# Max attepmts 
-attempNb=10
-
-# Select mystery number
-mystery=$(($RANDOM%1000+1))
-
-echo "Taper un nombre entre 1 et 1000 pour trouver le nombre mystÃ¨re."
-
-
-# Ch#!/bin/bash
-#
-# Author : P.Vuillaume
-#
-# Date : 20/03/2023
-#
+  GNU nano 6.2                                                                            guess.sh                                                                                     
 # Script : guess.sh
 #
 # Description : the script select a random integer (called mystery number) up to 1000.
-#	Player has 10 attempts to find this mystery number.
+#       Player has 10 attempts to find this mystery number.
 #
 # Input : none
 #
@@ -50,27 +35,29 @@ mystery=$(($RANDOM%1000+1))
 echo "Taper un nombre entre 1 et 1000 pour trouver le nombre mystÃ¨re."
 
 
+
 # Check if argument is valid (a single integer)
 for (( i=1; i <= $attempNb; ++i ))
 do
-	read input
-	if [[ $input ]] && [ $input -eq $input 2>/dev/null ] && [[ $input -ne 0 ]] 
-	then 
-		if [[ $input -lt $mystery ]]
-		then	
-			echo "Le nombre est plus grand."
-		elif  [[ $input -gt $mystery ]]
-		then
-			echo  "Le nombre est plus petit."
-		else
-			echo "Bravo !!!"
-			break
-		fi
-		if [[ $i -eq $attempNb ]]
-		then
-			echo "VOus avez perdu"
-		fi
-	else
-	     echo "Vous devez taper un nombre entre 1 et 1000"
-	fi
+        read input
+        if [[ $input ]] && [ $input -le 1000 ] && [[ $input -gt 0 ]] 
+        then 
+                if [[ $input -lt $mystery ]]
+                then    
+                        echo "Le nombre est plus grand."
+                elif  [[ $input -gt $mystery ]]
+                then
+                        echo  "Le nombre est plus petit."
+                else
+                        echo "Bravo !!!"
+                        break
+                fi
+        else
+             echo "Vous devez taper un nombre entre 1 et 1000"
+        fi
+        if [[ $i -eq $attempNb ]] && [[ $input != $mystery ]]
+        then
+                        echo "vous avez perdu"
+        fi
 done
+
