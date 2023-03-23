d0cd8d879108bec7ad07107c187a0fa313adb75c
sebastianplantardo <128522102+sebastianplantardo@users.noreply.github.com>
2023-03-23 16:54:56 +0100

    Update params.sh

--- params.sh
+++ params.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #auteur: seb
 #date: 20/03
-#description: Écrire un script qui affiche son nom, puis le chemin par lequel il a été appelé, puis tout ses arguments
+#description: Écrire un script qui affiche son nom, puis le chemin par lequel il a été appelé, puis>
 #un à un
 
 echo "nom: `basename $0`"
 echo "chemin: ` dirname $0`"
-echo $1
-echo $2
-echo $3
+
+count=1
+while [ $# -gt 0 ]; do
+    echo "argument $count : $1"
+    let count=$count+1
+   shift
+done
+
+
+
+
