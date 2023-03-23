e2169615c62b3c738188569ad72045b482db0e7a
sebastianplantardo <128522102+sebastianplantardo@users.noreply.github.com>
2023-03-23 17:19:13 +0100

    Update count.sh

--- count.sh
+++ count.sh
@@ -2,10 +2,16 @@
 #auteur: seb
 #date:
 #descriptif:script qui affiche les entiers de 1 à (argument)
+if [[ $1 = ?([-+])+([0-9]) ]] ; then
 
 i=1 # on initialise le compteur
 while [ $i -le $1 ]; do
   echo $i
   let $[ i+=1 ] # incremente i de 1 a chaque boucle
 done
+
+else
+        echo "entrez un nombre entier"
+fi
+
 
